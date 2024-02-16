7013f175583e461859976a0e0c2ea6f63ce484f4
XxTheRipperxX <53878146+XxTheRipperxX@users.noreply.github.com>
2024-02-16 17:11:46 +0100

    finishing

--- script.js
+++ script.js
@@ -17,15 +17,10 @@ const displayNumber = function (number) {
 };
 
 document.querySelector('.check').addEventListener('click', function () {
-  //note : usually whenever we get something from the user interface for exemlpe from an input field it usually always is a string  so w
-  const geuss = Number(document.querySelector('.guess').value); // el valeur illi bch nda5louh 8adi fil input bch yji as a string donc lezem nconvertih L number bch tsir el compairason mbin howa w el secret number illi na3mloulou generate mb3d(if we want to compare numbers with numbers wz need to first convert this string to a number  )
-  console.log(geuss, typeof geuss); // string // it should do the exact same thing as before but we first web stored that value into a variable
-  // note : usually in an aplication like this which has an input value from user the first thing to do is to check if there actually is a value and if there is no value well then we can print something to the console as a response
-
-  // when there is no input
+  const geuss = Number(document.querySelector('.guess').value);
+  console.log(geuss, typeof geuss);
 
   if (!geuss /*(or guess == 0)*/) {
-    // if there's no guess then print .... note: whenever i click this button we get "no number" message printed, this works because 0 is falsy value and so "guess"here is false but then we use the NOT operator to convert it to true and like this we can make this block of code execute
     document.querySelector('.message').textContent = 'No number ❌⛔';
 
     // when player wins
@@ -37,18 +32,12 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.highscore').textContent = highscore;
     }
 
-    // here we're selecting the body element to change the background color once if geuss === SecretNumber
     document.querySelector('body').style.backgroundColor = '#60b347';
-
-    // here we're selecting the number class to change the width
     document.querySelector('.number').style.width = '300px';
   } else if (geuss !== SecretNumber) {
     if (score > 1) {
-      // document.querySelector('.message').textContent =
-      //   geuss > SecretNumber ? 'too high !' : 'too low !';
       displayMessage(geuss > SecretNumber ? 'too high !' : 'too low !');
       score--;
-      // document.querySelector('.score').textContent = score;
       displayScore(score);
     } else {
       displayMessage('You lost❌');
@@ -56,30 +45,6 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('body').style.backgroundColor = 'rgb(255, 1, 0)';
     }
   }
-  //else if (geuss > SecretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = 'Too high !';
-
-  //     score--; // score = score - 1;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = 'you lost ❌⛔';
-  //     document.querySelector('body').style.backgroundColor = 'rgb(255, 1, 0)';
-  //     document.querySelector('.score').textContent = 0;
-  //   }
-
-  // } else if (geuss < SecretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = 'Too Low !';
-
-  //     score--; // score = score - 1;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = 'you lost ❌⛔';
-  //     document.querySelector('body').style.backgroundColor = 'rgb(255, 1, 0)';
-  //     document.querySelector('.score').textContent = 0;
-  //   }
-  // }
 });
 // Again button restore the initial values
 document.querySelector('.again').addEventListener('click', function () {
